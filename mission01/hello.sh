a2750c0636b3f15aea1eafa2332e12edce7bfff8
vikingeff <mail.leger@gmail.com>
2015-03-25 19:45:18 +0100

    changes

--- mission01/hello.sh
+++ mission01/hello.sh
@@ -1,2 +1,2 @@
 #docker run -t -i -p 80:80 hello/world:v42 /bin/bash
-docker run -t -i -u gleger -h 42born2code -p 45123:80 hello/world:v42 /bin/bash
+docker run -t -i -u gleger -h 42born2code -p 80:80 hello/world:v42 /bin/bash
