023b214cba8deb5b15c63e4a7f14979fc3f2f6f4
vikingeff <mail.leger@gmail.com>
2015-03-29 16:37:50 +0200

    update mission01

--- mission01/hello.sh
+++ mission01/hello.sh
@@ -1,2 +1,2 @@
 #docker run -t -i -p 80:80 hello/world:v42 /bin/bash
-docker run -t -i --name dock_nginx -u gleger -h 42born2code -p 80:80 hello/world:v42 /bin/bash
+docker run -t -i --name dock_nginx -p 80:80 hello/world:v42 /bin/bash
