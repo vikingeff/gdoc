e5a3a9e8e7cd6d902d693ccfed82af1f64384ca5
vikingeff <mail.leger@gmail.com>
2015-04-22 14:05:12 +0200

    rm useless

--- mission00/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#echo "export DOCKER_CERT_PATH=/nfs/zfs-student-2/users/2013/gleger/.boot2docker/certs/boot2docker-vm"
-#echo "export DOCKER_TLS_VERIFY=1"
-#echo "export DOCKER_HOST=tcp://192.168.59.103:2376"
-export DOCKER_HOST=tcp://192.168.59.103:2376
